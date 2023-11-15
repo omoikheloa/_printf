8249bf4d7f2a112b9e44d11b215a9ae304b0f32f
Omo <omoikheloa56@gmail.com>
2023-11-15 06:35:19 -0800

    updated code - main.h

--- main.h
+++ main.h
@@ -15,18 +15,11 @@
  * @fn: The function associated.
  */
 
-struct fmt
+typedef struct fmt
 {
-        char fmt;
-        int (*fn)(va_list, char[]);
-};
-
-
-/**
- * typedef struct fmt fmt_t - Struct op
- * @fmt: The format.
- * @fm_t: The function associated.
- */
+	char fmt;
+	int ((*fn)(va_list, char[]));
+}t;
 
 typedef struct fmt fmt_t;
 
@@ -42,19 +35,26 @@ int print_unsigned(va_list args_list, char buffer[]);
 int print_octal(va_list args_list, char buffer[]);
 int print_hexadecimal(va_list args_list, char buffer[]);
 int print_hexa_upper(va_list args_list, char buffer[]);
-
-int print_rot13string(va_list args_list, char buffer[], int flags, int width, int precision, int size);
-int print_reverse(va_list args_list, char buffer[], int flags, int width, int precision, int size);
-int print_pointer(va_list args_list, char buffer[], int flags, int width, int precision, int size);
-int print_non_printable(va_list args_list, char buffer[], int flags, int width, int precision, int size);
-int print_hexa(va_list args_list, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
-int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
-
+int print_rot13string(va_list args_list, char buffer[], 
+		int flags, int width, int precision, int size);
+int print_reverse(va_list args_list, char buffer[], int flags, 
+		int width, int precision, int size);
+int print_pointer(va_list args_list, char buffer[], int flags, 
+		int width, int precision, int size);
+int print_non_printable(va_list args_list, char buffer[], int flags, 
+		int width, int precision, int size);
+int print_hexa(va_list args_list, char map_to[], char buffer[], int flags, 
+		char flag_ch, int width, int precision, int size);
+int handle_write_char(char c, char buffer[], int flags, int width, 
+		int precision, int size);
+int write_number(int is_positive, int ind, char buffer[], int flags, 
+		int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, 
+		int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, 
+		char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, 
+		int precision, int size);
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
